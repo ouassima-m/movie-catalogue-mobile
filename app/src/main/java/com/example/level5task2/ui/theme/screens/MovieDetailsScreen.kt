d48dbce78cea50ec3ee81d191c1d0b5152a9e3e2
ouassima <madmaro@hva.nl>
2024-10-10 22:09:04 +0200

    detailscreen ui

--- app/src/main/java/com/example/level5task2/ui/theme/screens/MovieDetailsScreen.kt
+++ app/src/main/java/com/example/level5task2/ui/theme/screens/MovieDetailsScreen.kt
@@ -3,24 +3,33 @@ package com.example.level5task2.ui.theme.screens
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -44,6 +53,9 @@ fun MovieDetailsScreen(
                 modifier= Modifier.padding(innerPadding),
                 movieDetails = movieDetails
             )
+        },
+        bottomBar = {
+            DetailsAddButton()
         }
     )
 }
@@ -51,40 +63,91 @@ fun MovieDetailsScreen(
 
 @Composable
 fun DetailsScreenContent(modifier: Modifier, movieDetails: Movies? = null) {
-    Column (
-        modifier = modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.SpaceAround,
 
-    ){
         if (movieDetails != null) {
             Column(
             ) {
                 Image(
-                    painter = rememberAsyncImagePainter(model = "https://image.tmdb.org/t/p/w500${movieDetails.headerIMG}"),
+                    painter = rememberAsyncImagePainter(
+                        model = "https://image.tmdb.org/t/p/w500${movieDetails.headerIMG}"
+                    ),
                     contentDescription = movieDetails.title,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(350.dp)
+                        .height(230.dp)
                 )
-                Text(text = "Release Date: ${movieDetails.releaseDate}")
-                Text(text = "Rating: ${movieDetails.rating}")
-                Text(text = movieDetails.overview)
+                Row (
+                    modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Image(
+                        painter = rememberAsyncImagePainter(
+                            model = "https://image.tmdb.org/t/p/w500${movieDetails.posterIMG}"
+                        ),
+                        contentDescription = movieDetails.title,
+                        modifier = Modifier
+                            .height(230.dp).clip(RoundedCornerShape(10.dp))
+                    )
+                    Box (
+                        modifier = Modifier.padding(16.dp).fillMaxWidth().height(230.dp)
+                    ) {
+                        Text(
+                            text = movieDetails.title,
+                            style = MaterialTheme.typography.headlineMedium,
+                            modifier = Modifier.padding(4.dp).align(Alignment.Center)
+                        )
+                        Row (
+                            modifier = Modifier.padding(4.dp).align(Alignment.BottomStart)
+
+                        ){
+                            Icon(
+                                imageVector = Icons.Default.Star,
+                                contentDescription = "star",
+                                modifier = Modifier
+                            )
+                            Text(
+                                text = movieDetails.rating.toString(),
+                                modifier = Modifier.padding(4.dp)
+                            )
+                        }
+                    }
+                }
+                Text(
+                    text = stringResource(R.string.overview),
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.padding(12.dp)
+                )
+
+                LazyColumn (
+                    modifier = Modifier.padding(bottom = 94.dp)
+                ) {
+                    item{
+                        Text(
+                            text = movieDetails.overview,
+                            style = TextStyle(fontSize = 18.sp),
+                            modifier = Modifier.padding(12.dp)
+                        )
+                    }
+                }
             }
         } else {
             Text(text = "Movie details not available.", modifier = Modifier.padding(16.dp))
         }
-
-        DetailsAddButton()
     }
-}
+
 
 
 @Composable
 fun DetailsAddButton() {
     Column(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
-        Text(text = stringResource(R.string.add_and_remove_fav))
+        Text(
+            text = stringResource(R.string.add_and_remove_fav),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(6.dp)
+        )
 
 
         Button(
